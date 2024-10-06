4375eb4da503d01c90a5ced2ec77256c8188d23c
piyush wandile <piyushwandile134@gmail.com>
2024-10-06 13:27:13 +0530

    Hands on python

---
+++ Basics-01/main.py
@@ -0,0 +1,31 @@
+from loguru import logger
+
+# Python Output
+
+# Different lines
+print("hello")
+print("world")
+
+# Backlash n in python ...
+
+print("hello", end=" ")
+print("world")
+
+
+# Data Types
+
+# Integer
+print(8)
+# 1*10^308
+print(1e309)
+
+# Decimal/Float
+print(8.55)
+print(1.7e309)
+
+# Boolean
+print(True)
+print(False)
+
+# Text/String
+print('Hello World')
