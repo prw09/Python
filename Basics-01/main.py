83611fcb50ce825151f9526f618ae0d4450782bf
piyush wandile <piyushwandile134@gmail.com>
2024-10-06 14:03:21 +0530

    basics all commits

--- Basics-01/main.py
+++ Basics-01/main.py
@@ -29,3 +29,50 @@ print(False)
 
 # Text/String
 print('Hello World')
+
+# complex
+print(5+6j)
+
+
+# Type functions ....
+
+print(type(3))
+print(type(3.5))
+print(type("hello"))
+
+
+#  Variables in python
+#  for used for storing the data which have a relevant information
+
+        ## Static Vs Dynamic Typing
+        ## Static Vs Dynamic Binding
+        ## stylish declaration techniques
+
+
+# Dynamic typing in python
+
+a = 5    # here the dynamic typing where u don't declare the type of variable
+
+
+# Keywords and Identifiers
+
+# Identifiers
+# You can't start with a digit
+name1 = 'Piyush'
+print(name1)
+# You can use special chars -> _
+_ = 'Piyush'
+print(_)
+# identifiers can not be keyword
+
+
+# Implicit Vs Explicit
+print(5+5.6)
+print(type(5), type(5.6))
+
+# print(4 + '4')
+
+# Explicit
+# str -> int
+#int(4+5j)
+
