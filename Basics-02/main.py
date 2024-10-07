9cdcfea24175ced62c0ecf2cfb6ff3e4d4ec12f4
piyush wandile <piyushwandile134@gmail.com>
2024-10-07 11:29:47 +0530

    3rd commit

--- Basics-02/main.py
+++ Basics-02/main.py
@@ -4,20 +4,19 @@ logger.info("hello")
 
 # Arithmetic operators
 
-logger.info(5+6)
+logger.info(5 + 6)
 
-logger.info(5-6)
+logger.info(5 - 6)
 
-logger.info(5*6)
+logger.info(5 * 6)
 
-logger.info(5/2)
+logger.info(5 / 2)
 
-logger.info(5//2)
+logger.info(5 // 2)
 
 logger.info(5 % 2)
 
-logger.info(5**2)
-
+logger.info(5 ** 2)
 
 # Relational operators
 
@@ -28,7 +27,6 @@ logger.info(5 < 6)
 logger.info(5 == 6)
 
 logger.info(5 != 6)
-
 
 # Logical operators
 
@@ -50,3 +48,37 @@ logger.info(3 ^ 3)  # If same then it is 0
 
 logger.info(3 ^ 2)  # if different then it is 1
 
+logger.info(~3)
+
+logger.info(~3)
+
+# assignment operator
+
+a = 2  # here = is an assignment operator
+
+a %= 2
+
+# membership operator
+
+logger.info("f" in "DELHI")
+
+# problem 01
+# find the sum of 3 digits numbers entered by the user
+
+num = int(input("Enter the 3-digits number"))
+
+a = num % 10
+
+num = num // 10
+
+b = num % 10
+
+num = num // 10
+
+c = num % 10
+
+num = num // 10
+
+d = a, b, c
+
+logger.info(f"The number is {d}")
