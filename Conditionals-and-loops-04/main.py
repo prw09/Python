73171cdfc9812cb5ecd33a0e72b731e611f81952
piyush wandile <piyushwandile134@gmail.com>
2024-10-10 13:28:45 +0530

    modules knowledge

--- Conditionals-and-loops-04/main.py
+++ Conditionals-and-loops-04/main.py
@@ -1,4 +1,8 @@
 import logging
+import math
+import keyword
+import datetime
+
 
 # Set up logging with a simpler format
 logging.basicConfig(format='%(message)s', level=logging.INFO)
@@ -7,50 +11,50 @@ logger = logging.getLogger()
 # Login and Indentation
 # user is asked for email and password
 
-# email = input("Enter your email: ")
-# password = input("Enter your password: ")
-#
-# if email == 'piyush@gmail.com' and password == 'Piyush@1234':
-#     logger.info("Welcome to the webpage!")
-#
-# elif email == "piyush@gmail.com" and password != 'Piyush@1234':
-#     logger.info("Your entered a incorrect password!")
-#     password = input("Enter password")
-#
-#     if password != 'Piyush@1234':
-#         logger.info("Your entered a incorrect password!")
-#     else:
-#         logger.info("Welcome to the webpage!")
-#
-# else:
-#     logger.info("please correct your email or password!")
-#
-#
-# # Better version
-#
-# # Get email and password input
-# email = input("Enter your email: ")
-# password = input("Enter your password: ")
-#
-# # Define correct email and password
-# correct_email = 'piyush@gmail.com'
-# correct_password = 'Piyush@1234'
-#
-# # Authentication logic
-# if email == correct_email and password == correct_password:
-#     logger.info("Welcome to the webpage!")
-#
-# elif email == correct_email and password != correct_password:
-#     logger.info("You entered an incorrect password!")
-#     password = input("Enter password: ")
-#
-#     if password == correct_password:
-#         logger.info("Welcome to the webpage!")
-#     else:
-#         logger.info("You entered an incorrect password again!")
-#
-# else:
-#     logger.info("Please correct your email or password!")
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+
+if email == 'piyush@gmail.com' and password == 'Piyush@1234':
+    logger.info("Welcome to the webpage!")
+
+elif email == "piyush@gmail.com" and password != 'Piyush@1234':
+    logger.info("Your entered a incorrect password!")
+    password = input("Enter password")
+
+    if password != 'Piyush@1234':
+        logger.info("Your entered a incorrect password!")
+    else:
+        logger.info("Welcome to the webpage!")
+
+else:
+    logger.info("please correct your email or password!")
+
+
+# Better version
+
+# Get email and password input
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+
+# Define correct email and password
+correct_email = 'piyush@gmail.com'
+correct_password = 'Piyush@1234'
+
+# Authentication logic
+if email == correct_email and password == correct_password:
+    logger.info("Welcome to the webpage!")
+
+elif email == correct_email and password != correct_password:
+    logger.info("You entered an incorrect password!")
+    password = input("Enter password: ")
+
+    if password == correct_password:
+        logger.info("Welcome to the webpage!")
+    else:
+        logger.info("You entered an incorrect password again!")
+
+else:
+    logger.info("Please correct your email or password!")
 
 
 # Min of 3 numbers problem
@@ -67,3 +71,31 @@ elif second_num < first_num and second_num < third_num:
 
 else:
     logger.info(f"third_num is smallest number!, {third_num}")
+
+
+# Menu driven calculator
+
+fnum = int(input("Enter the first number"))
+snum = int(input("Enter the second number"))
+
+op = input("Enter the operations=")
+
+if op == '+':
+    print(fnum + snum)
+elif op == '-':
+    print(fnum - snum)
+elif op == '*':
+    print(fnum * snum)
+elif op == '/':
+    print(fnum / snum)
+else:
+    print("")
+
+
+print(math.factorial(5))
+
+print(keyword.kwlist)
+
+print(datetime.datetime.now())
+
+print(help('modules'))
