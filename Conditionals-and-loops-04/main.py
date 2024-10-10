605f2c0dac6065791639de5020346bf837711bac
piyush wandile <piyushwandile134@gmail.com>
2024-10-10 11:54:48 +0530

    Login and Indentation using if and else

--- Conditionals-and-loops-04/main.py
+++ Conditionals-and-loops-04/main.py
@@ -1,6 +1,11 @@
-from loguru import logger
+import logging
 
-# Login and indentation
+# Set up logging with a simpler format
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+logger = logging.getLogger()
+
+
+# Login and Indentation
 # user is asked for email and password
 
 email = input("Enter your email: ")
@@ -9,6 +14,41 @@ password = input("Enter your password: ")
 if email == 'piyush@gmail.com' and password == 'Piyush@1234':
     logger.info("Welcome to the webpage!")
 
+elif email == "piyush@gmail.com" and password != 'Piyush@1234':
+    logger.info("Your entered a incorrect password!")
+    password = input("Enter password")
+
+    if password != 'Piyush@1234':
+        logger.info("Your entered a incorrect password!")
+    else:
+        logger.info("Welcome to the webpage!")
+
 else:
     logger.info("please correct your email or password!")
 
+
+# Better version
+
+# Get email and password input
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+
+# Define correct email and password
+correct_email = 'piyush@gmail.com'
+correct_password = 'Piyush@1234'
+
+# Authentication logic
+if email == correct_email and password == correct_password:
+    logger.info("Welcome to the webpage!")
+
+elif email == correct_email and password != correct_password:
+    logger.info("You entered an incorrect password!")
+    password = input("Enter password: ")
+
+    if password == correct_password:
+        logger.info("Welcome to the webpage!")
+    else:
+        logger.info("You entered an incorrect password again!")
+
+else:
+    logger.info("Please correct your email or password!")
