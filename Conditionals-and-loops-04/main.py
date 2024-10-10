f2e1ac9d2f481f6739c211f8473821c8043aa61f
piyush wandile <piyushwandile134@gmail.com>
2024-10-10 11:06:47 +0530

    conditionals first commit

--- Conditionals-and-loops-04/main.py
+++ Conditionals-and-loops-04/main.py
@@ -1,1 +1,14 @@
-print(4)
+from loguru import logger
+
+# Login and indentation
+# user is asked for email and password
+
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+
+if email == 'piyush@gmail.com' and password == 'Piyush@1234':
+    logger.info("Welcome to the webpage!")
+
+else:
+    logger.info("please correct your email or password!")
+
